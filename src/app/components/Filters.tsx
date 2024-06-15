578b77d37aff67374bba2e4459d4a7093d037a0a
Хамзат Хамзатов <khamzat@MacBook-Pro-Hamzat.local>
2024-06-15 18:24:29 +0300

    second

--- src/app/components/Filters.tsx
+++ src/app/components/Filters.tsx
@@ -1,9 +1,9 @@
-'use client';
+'use client'; // Обозначаем этот компонент как клиентский
 
 import { useState, useEffect } from 'react';
 import { getFilters } from '../lib/api';
-import { Form, Accordion, Card } from 'react-bootstrap';
-import styles from './Filters.module.css';
+import { Form, Accordion } from 'react-bootstrap';
+import styles from './Filters.module.css'; // импортируем стили
 
 type FilterProps = {
   onChange: (filters: any) => void;
@@ -29,7 +29,10 @@ const Filters: React.FC<FilterProps> = ({ onChange }) => {
         const brands = Array.isArray(data.brands.values) ? data.brands.values : [];
         const allModels = Array.isArray(data.models.values) ? data.models.values : [];
         const tariffsObj = data.tarif?.values || {};
-        const tariffs = Object.entries(tariffsObj).map(([id, name]) => ({ id, name }));
+        const tariffs = Object.entries(tariffsObj).map(([id, name]) => ({
+          id,
+          name: name as string,
+        }));
 
         const modelsGroupedByBrand = allModels.reduce((acc: any, modelGroup: any) => {
           acc[modelGroup.brand] = modelGroup.models;
@@ -41,6 +44,7 @@ const Filters: React.FC<FilterProps> = ({ onChange }) => {
         setModels(modelsGroupedByBrand);
         setTariffs(tariffs);
       } catch (error) {
+        console.error('Error fetching filters:', error);
         setBrands([]);
         setAllModels([]);
         setModels({});
@@ -52,22 +56,11 @@ const Filters: React.FC<FilterProps> = ({ onChange }) => {
 
   const handleCheckboxChange = (key: string, value: string) => {
     const updatedFilters = { ...selectedFilters };
-
-    if (value === 'Все') {
-      updatedFilters[key] = [];
+    if (updatedFilters[key].includes(value)) {
+      updatedFilters[key] = updatedFilters[key].filter((item: string) => item !== value);
     } else {
-      if (updatedFilters[key].includes(value)) {
-        updatedFilters[key] = updatedFilters[key].filter(
-          (item: string) => item !== value
-        );
-      } else {
-        updatedFilters[key] = updatedFilters[key].filter(
-          (item: string) => item !== 'Все'
-        );
-        updatedFilters[key].push(value);
-      }
+      updatedFilters[key].push(value);
     }
-
     setSelectedFilters(updatedFilters);
     console.log('Updated filters:', updatedFilters);
     onChange(updatedFilters);
@@ -83,93 +76,85 @@ const Filters: React.FC<FilterProps> = ({ onChange }) => {
 
   return (
     <div className={styles.filters}>
-      <Card>
-        <Card.Header className={styles.header}>Фильтры</Card.Header>
-        <Card.Body>
-          <Accordion defaultActiveKey={['1', '2', '3']} alwaysOpen>
-            <Accordion.Item eventKey="1">
-              <Accordion.Header>Марки</Accordion.Header>
-              <Accordion.Body>
-                <Form.Group controlId="formBrands">
-                  <Form.Check
-                    type="checkbox"
-                    label="Все"
-                    key="Все"
-                    value="Все"
-                    checked={selectedFilters.brand.length === 0}
-                    onChange={e => handleCheckboxChange('brand', 'Все')}
-                  />
-                  {brands.map((brand: string) => (
+      <Accordion defaultActiveKey={['1', '2', '3']} alwaysOpen>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Марки</Accordion.Header>
+          <Accordion.Body>
+            <Form.Group controlId="formBrands">
+              <Form.Check
+                type="checkbox"
+                label="Все"
+                value="Все"
+                onChange={() => handleCheckboxChange('brand', 'Все')}
+                checked={selectedFilters.brand.length === 0}
+              />
+              {brands.map((brand: string) => (
+                <Form.Check
+                  type="checkbox"
+                  label={brand}
+                  key={brand}
+                  value={brand}
+                  onChange={e => handleCheckboxChange('brand', e.target.value)}
+                  checked={selectedFilters.brand.includes(brand)}
+                />
+              ))}
+            </Form.Group>
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="2">
+          <Accordion.Header>Модели</Accordion.Header>
+          <Accordion.Body>
+            <Form.Group controlId="formModels">
+              <Form.Check
+                type="checkbox"
+                label="Все"
+                value="Все"
+                onChange={() => handleCheckboxChange('model', 'Все')}
+                checked={selectedFilters.model.length === 0}
+              />
+              {Object.keys(models).map((brand: string) => (
+                <div key={brand}>
+                  <h5>{brand}</h5>
+                  {models[brand].map((model: string) => (
                     <Form.Check
                       type="checkbox"
-                      label={brand}
-                      key={brand}
-                      value={brand}
-                      checked={selectedFilters.brand.includes(brand)}
-                      onChange={e => handleCheckboxChange('brand', e.target.value)}
+                      label={model}
+                      key={model}
+                      value={model}
+                      onChange={e => handleCheckboxChange('model', e.target.value)}
+                      checked={selectedFilters.model.includes(model)}
                     />
                   ))}
-                </Form.Group>
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="2">
-              <Accordion.Header>Модели</Accordion.Header>
-              <Accordion.Body>
-                <Form.Group controlId="formModels">
-                  <Form.Check
-                    type="checkbox"
-                    label="Все"
-                    key="Все модели"
-                    value="Все"
-                    checked={selectedFilters.model.length === 0}
-                    onChange={e => handleCheckboxChange('model', 'Все')}
-                  />
-                  {Object.keys(models).map((brand: string) => (
-                    <div key={brand}>
-                      <h5>{brand}</h5>
-                      {models[brand].map((model: string) => (
-                        <Form.Check
-                          type="checkbox"
-                          label={model}
-                          key={model}
-                          value={model}
-                          checked={selectedFilters.model.includes(model)}
-                          onChange={e => handleCheckboxChange('model', e.target.value)}
-                        />
-                      ))}
-                    </div>
-                  ))}
-                </Form.Group>
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="3">
-              <Accordion.Header>Тарифы</Accordion.Header>
-              <Accordion.Body>
-                <Form.Group controlId="formTariffs">
-                  <Form.Check
-                    type="checkbox"
-                    label="Все"
-                    key="Все тарифы"
-                    value="Все"
-                    checked={selectedFilters.tarif.length === 0}
-                    onChange={e => handleCheckboxChange('tarif', 'Все')}
-                  />
-                  {tariffs.map(tarif => (
-                    <Form.Check
-                      type="checkbox"
-                      label={tarif.name}
-                      key={tarif.id}
-                      value={tarif.id}
-                      checked={selectedFilters.tarif.includes(tarif.id)}
-                      onChange={e => handleCheckboxChange('tarif', e.target.value)}
-                    />
-                  ))}
-                </Form.Group>
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-        </Card.Body>
-      </Card>
+                </div>
+              ))}
+            </Form.Group>
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="3">
+          <Accordion.Header>Тарифы</Accordion.Header>
+          <Accordion.Body>
+            <Form.Group controlId="formTariffs">
+              <Form.Check
+                type="checkbox"
+                label="Все"
+                value="Все"
+                onChange={() => handleCheckboxChange('tarif', 'Все')}
+                checked={selectedFilters.tarif.length === 0}
+              />
+              {tariffs.map(tarif => (
+                <Form.Check
+                  type="checkbox"
+                  label={tarif.name}
+                  key={tarif.id}
+                  value={tarif.id}
+                  onChange={e => handleCheckboxChange('tarif', e.target.value)}
+                  checked={selectedFilters.tarif.includes(tarif.id)}
+                />
+              ))}
+            </Form.Group>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
     </div>
   );
 };
